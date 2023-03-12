723950b78c054055c31c97fe0969040c9c98bdf8
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-03-12 18:58:22 +0100

    Poprawione rozwiązanie

--- src/main/java/pl/javastart/task/TaxCalculate.java
+++ src/main/java/pl/javastart/task/TaxCalculate.java
@@ -1,15 +1,15 @@
 package pl.javastart.task;
 
 public class TaxCalculate {
-    private double tax23 = 0.23;
-    private double tax8 = 0.08;
-    private double tax5 = 0.05;
+    private static final double TAX_23 = 0.23;
+    private static final double TAX_8 = 0.08;
+    private static final double TAX_5 = 0.05;
 
     double taxPrice(Product product) {
         double price = switch (product.getCategory()) {
-            case "Usługi" -> product.getPriceNet() * (1 + tax23);
-            case "Produkt spożywczy" -> product.getPriceNet() * (1 + tax5);
-            default -> product.getPriceNet() * (1 + tax8);
+            case "Usługi" -> product.getPriceNet() * (1 + TAX_23);
+            case "Produkt spożywczy" -> product.getPriceNet() * (1 + TAX_5);
+            default -> product.getPriceNet() * (1 + TAX_8);
         };
         return price;
     }
